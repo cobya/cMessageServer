5c766326876f77ba1fcacfa02ec3c70cb8eee972
Coby Allred <cobyallred@gmail.com>
2019-04-24 21:57:17 -0700

    Enable CORS

--- cMessageServer/routes/index.js
+++ cMessageServer/routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+// enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/../public/home.html'));
